a2185352d6005d30e26df3a93a8af05f61ac8de5
Devid <nnick4673@gmail.com>
2022-08-12 16:19:21 -0300

    Add script de estudo 1

---
+++ study_shell_script/script1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nome="Devid"
+idade=17
+cidade_natal="Roraima"
+estado="AM"
+
+echo "Meu nome é $nome"
+echo "Tenho $idade anos"
+echo "Nasci em $cidade_natal"
+echo "No estado $estado"
